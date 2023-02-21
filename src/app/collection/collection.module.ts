51a6088e595648fbc7f016c77b89b8fb4ceee727
gosai09 <gosai09@gmail.com>
2023-02-21 15:58:56 -0500

    collection home component create in collection mod

--- src/app/collection/collection.module.ts
+++ src/app/collection/collection.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionRoutingModule } from './collection-routing.module';
+import { CollectionHomeComponent } from './collection-home/collection-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CollectionHomeComponent
+  ],
   imports: [
     CommonModule,
     CollectionRoutingModule
