48df43b38f831a63d367b33d8e7a7cf441653d63
gosai09 <gosai09@gmail.com>
2023-02-22 16:11:23 -0500

    child routing in collection module with tab

--- src/app/collection/collection-routing.module.ts
+++ src/app/collection/collection-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component';
 import { CollectionHomeComponent } from './collection-home/collection-home.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CollectionHomeComponent,
+    children: [
+      {
+        path: '',
+        component: BiographyComponent,
+      },
+
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent,
+      },
+    ],
   },
 ];
 
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
-    ViewsHomeComponent
+    ViewsHomeComponent,
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
